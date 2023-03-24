57d48245e23e3c3ae41cc2217085e7de1c7c5101
Emmanuel Makokha <99004575+MamaiTheCoder@users.noreply.github.com>
2023-03-24 22:11:57 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# Installs puppet-lint, version 2.1.0
+# Installs flask version 2.1.0 from pip3
 
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install-flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => '/usr/local/bin:/usr/bin:/bin',
+  unless  => '/usr/bin/pip3 freeze | grep flask==2.1.0',
 }
